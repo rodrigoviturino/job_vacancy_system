75a4b2c3fec3c5155db79c74a52b7c53574e8c3c
Rodrigo Viturino <Viturino_souza@outlook.com>
2020-07-24 18:56:05 -0300

    Finalização do projeto com as seguintes funcionalidades: listagem de vagas, adição de novas vagas e campo de pesquisa. utilizando recursos do node e banco de dados orm

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
 const app = express();
+const path = require("path");
 const db = require("./db/connection");
 const bodyParser = require("body-parser");
 
-const PORT = 3000;
+const Job = require("./models/Job");
+const Sequelize = require("sequelize");
+const sequelize = require("./db/connection");
+const Op = Sequelize.Op;
 
-// Routes
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const PORT = 3000;
+/******************************************************************************** */
+app.listen(PORT, () => {
+  console.log(`Express está rodando na porta ${PORT}`);
 });
 
+/****** BODY PARSER ******/
 // temos que dizer que vamos usar o BodyParser
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// Handle Bars
+
+// Setamos o path para pegar a raiz do projeto e juntando com o diretorio VIEWS
+app.set("views", path.join(__dirname, "views"));
+// definindo o template principal da aplicação
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// Definindo o motor responsável por renderizar o FRONT
+app.set("view engine", "handlebars");
+
+//*** Setando a pasta que vai ser STATIC, para o CSS e JS ser linkados */
+app.use(express.static(path.join(__dirname, "public")));
+
+/******************************************************************************** */
 
 // db Connection
 // sequelize retorna uma Promise
@@ -23,6 +44,35 @@ db.authenticate()
     console.log("Ocorreu um erro ao conectar", erro);
   });
 
-app.listen(PORT, () => {
-  console.log(`Express está rodando na porta ${PORT}`);
+/******************************************************************************** */
+
+// Routes
+app.get("/", (req, res) => {
+  // .job é o NAME do INPUT, então estamos fazendo
+  let search = req.query.job;
+  // faz o seguinte ao digitar PH, Word, Press, ele já completa a busca
+  let query = "%" + search + "%";
+
+  if (!search) {
+    Job.findAll()
+      .then((jobs) => {
+        res.render("index", { jobs });
+      })
+      .catch((erro) => console.log(erro));
+  } else {
+    Job.findAll({
+      where: {
+        title: { [Op.like]: query },
+      },
+    })
+      .then((jobs) => {
+        res.render("index", { jobs, search });
+      })
+      .catch((erro) => console.log(erro));
+  }
 });
+
+// Jobs Routes
+app.use("/jobs", require("./routes/jobs"));
+
+/******************************************************************************** */
--- routes/jobs.js
+++ routes/jobs.js
@@ -2,11 +2,24 @@ const express = require("express");
 const router = express.Router();
 const Job = require("../models/Job");
 
+router.get("/view/:id", (req, res) => {
+  Job.findOne({
+    where: {
+      id: req.params.id,
+    },
+  }).then((job) => {
+    res.render("view", { job });
+  });
+});
+
 // Tivemos que ir no app e declar o bodyParser para pegar os dados do "corpo da requisição" e preencher as variaveis do req.body
+router.get("/add", (req, res) => {
+  res.render("add");
+});
 
 // add job via post
 router.post("/add", (req, res) => {
-  let { title, salary, company, description, email, new_job } = req.body;
+  let { title, description, salary, company, email, new_job } = req.body;
 
   // Insert - Método do Sequelize e vai retornar uma Promise
   Job.create({
@@ -20,3 +33,5 @@ router.post("/add", (req, res) => {
     .then(() => res.redirect("/"))
     .catch((erro) => console.log(erro));
 });
+
+module.exports = router;
